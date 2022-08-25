5e72786ffa082e88a7e216ede73f286f0ae67726
Cassio <me.me@me.com>
2022-08-25 12:02:08 -0300

    Adding a new directory, 2oDesafio, and webServerProvisioning.sh

---
+++ 2oDesafio/webServerProvisioning.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "This script automatizes the process of updating Ubuntu, installing Apache2, installing Unzip, downloading and deploying an application on the default Apache directory."
+
+echo "Updating Ubuntu"
+
+apt update -y
+apt upgrade -y
+
+echo "Installing Apache2"
+
+apt install apache2 -y
+
+echo "Installing Unzip"
+
+apt install unzip -y
+
+echo "Downloading and deploying the application on Apache directory"
+
+wget -P /tmp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip -d /var/www/html
+
+rm -rf main.zip
+
+echo "Process successfully concluded"
