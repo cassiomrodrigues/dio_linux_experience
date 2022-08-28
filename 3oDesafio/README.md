327f55f17e17b3faccabe9847b87499846259abf
Cassio <cassiorodrigues@me.com>
2022-08-28 15:27:19 -0300

    Adicionando o diretorio com conteudo do 3oDesafio

---
+++ 3oDesafio/README.md
@@ -0,0 +1,4 @@
+Docker: Utilização prática no cenário de Microsserviços
+Originalmente criado por Denilson Bonatti, Instrutor - Digital Innovation One
+
+Esté é um "fork" do projeto apresentado pelo instrutor Denilson durante o Bootcamp - Linux Experience, da DIO.
---
+++ 3oDesafio/index.php
@@ -0,0 +1,50 @@
+<html>
+
+<head>
+<title>Exemplo PHP</title>
+</head>
+<body>
+
+<?php
+ini_set("display_errors", 1);
+header('Content-Type: text/html; charset=iso-8859-1');
+
+
+
+echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+
+//Altere a informacao XXX.XXX.XXX.XXX abaixo para o endereço de IP do servidor MySQL
+$servername = "XXX.XXX.XXX.XXX";
+$username = "root";
+$password = "Senha123";
+$database = "meubanco";
+
+// Criar conexão
+
+
+$link = new mysqli($servername, $username, $password, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+//Atribuindo valores randonicos às variaveis valor_rand1 e valor_rand2
+$valor_rand1 =  rand(1, 999);
+$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$host_name = gethostname();
+
+//Inserindo valores na tabela dados
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+
+
+if ($link->query($query) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $link->error;
+}
+
+?>
+</body>
+</html>
